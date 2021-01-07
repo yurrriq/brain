4cdb7fe22037c74974d9aaf61bbdec00e158fd87
Eric Bailey <eric@ericb.me>
2021-01-06 23:17:54 -0600

    Add commutative diagram of category associativity

---
+++ category-theory/associativity.md
@@ -0,0 +1,3 @@
+Given [[morphism|morphisms]] $\mor{f}{A}{B}$, $\mor{g}{B}{C}$, and $\mor{h}{C}{D}$, the following [[diagram]] commutes.
+
+![Commutative diagram of category associativity|300](./svg/category-associativity.svg)
--- shell.nix
+++ shell.nix
@@ -2,9 +2,22 @@
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
+    gnumake
     niv
     nixpkgs-fmt
     obsidian
+    pdf2svg
     python3Packages.pre-commit
+    (
+      texlive.combine {
+        inherit (texlive) scheme-small
+          currfile
+          filemod
+          latexmk
+          standalone
+          tikz-cd
+          ;
+      }
+    )
   ];
 }
