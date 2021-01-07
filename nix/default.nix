878f9b39eaf0134219dfbbfe620298c88a1a6c86
Eric Bailey <eric@ericb.me>
2021-01-07 00:11:57 -0600

    obsidian: 0.10.1 -> 0.10.6

--- nix/default.nix
+++ nix/default.nix
@@ -1,1 +1,17 @@
-import (import ./sources.nix).nixpkgs { }
+let
+  sources = import ./sources.nix;
+in
+import sources.nixpkgs {
+  overlays = [
+    (_: pkgs: {
+      obsidian =
+        let
+          src = sources.obsidian;
+        in
+        pkgs.obsidian.overrideAttrs (_: rec {
+          inherit (src) version;
+          inherit src;
+        });
+    })
+  ];
+}
