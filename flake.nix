6851d9b92d2382f65c151a211bfc5631f6c8b10d
Eric Bailey <eric@ericb.me>
2023-02-25 22:48:16 -0600

    nix flake update

--- flake.nix
+++ flake.nix
@@ -2,48 +2,57 @@
   description = "Stuff I know";
 
   inputs = {
-    emacs-overlay.url = "github:nix-community/emacs-overlay";
+    emacs-overlay = {
+      inputs = {
+        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
+      };
+      url = "github:nix-community/emacs-overlay";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs";
   };
 
-  outputs = { self, emacs-overlay, nixpkgs }:
-    let
-      pkgs = import nixpkgs {
-        overlays = [
-          emacs-overlay.overlay
-        ];
-        system = "x86_64-linux";
-      };
-    in
-    {
-      devShell.x86_64-linux = with pkgs; mkShell {
-        buildInputs = [
-          (
-            emacsWithPackagesFromUsePackage {
-              alwaysEnsure = true;
-              config = ./emacs.el;
-              extraEmacsPackages = epkgs: [
-                epkgs.org-roam-ui
-              ];
-            }
-          )
-          gitAndTools.pre-commit
-          gnumake
-          nixpkgs-fmt
-          poppler_utils
-          (
-            texlive.combine {
-              inherit (texlive) scheme-small
-                currfile
-                dvipng
-                filemod
-                latexmk
-                standalone
-                tikz-cd
-                ;
-            }
-          )
-        ];
-      };
-    };
+  outputs = { emacs-overlay, flake-utils, nixpkgs, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          overlays = [
+            emacs-overlay.overlay
+          ];
+          inherit system;
+        };
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            (
+              emacsWithPackagesFromUsePackage {
+                alwaysEnsure = true;
+                config = ./emacs.el;
+                extraEmacsPackages = epkgs: [
+                  epkgs.org-roam-ui
+                ];
+              }
+            )
+            gnumake
+            graphviz
+            nixpkgs-fmt
+            poppler_utils
+            pre-commit
+            (
+              texlive.combine {
+                inherit (texlive) scheme-small
+                  currfile
+                  dvipng
+                  filemod
+                  latexmk
+                  standalone
+                  tikz-cd
+                  ;
+              }
+            )
+          ];
+        };
+      });
 }
