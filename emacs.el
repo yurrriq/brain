a5050b3c7558c39aad745704058225e93a8a2756
Eric Bailey <eric@ericb.me>
2024-01-02 01:11:46 -0600

    fix(emacs): prefer emacs.el over .dir-locals.el

--- emacs.el
+++ emacs.el
@@ -82,12 +82,46 @@
                  :target (file+head "${slug}.org"
                                     "#+title: ${title}\n")
                  :unnarrowed t))
-    (org-roam-setup)
+  (org-roam-setup)
+  ;; TODO
+  ;; (add-hook 'org-roam-graph-generation-hook
+  ;;           (lambda (dot svg)
+  ;;             (let ((publishing-directory
+  ;;                    (plist-get (cdr (assoc "category-theory-html" org-publish-project-alist))
+  ;;                               :publishing-directory)))
+  ;;               (copy-file svg (concat publishing-directory "/sitemap.svg") 't)
+  ;;               ;; (kill-buffer (file-name-nondirectory svg))
+  ;;               )))
   ;; https://github.com/org-roam/org-roam-ui/issues/236
   (setq
    org-roam-directory (expand-file-name (concat default-directory "category-theory/"))
    org-roam-db-location (expand-file-name "org-roam.db" org-roam-directory)
-   org-id-locations-file (expand-file-name ".org-id-locations" org-roam-directory)))
+   org-id-locations-file (expand-file-name ".org-id-locations" org-roam-directory)
+   org-publish-project-alist
+   `(("category-theory-html"
+      :base-directory       ,org-roam-directory
+      :auto-sitemap         t
+      :sitemap-filename     "index.org"
+      ;; FIXME:
+      ;; :sitemap-function     eb/org-roam-sitemap
+      :sitemap-title        "Category Theory"
+      :publishing-directory ,(file-name-concat default-directory "docs")
+      :publishing-function  eb/org-roam-publish-to-html
+      :fontify-natively     t
+      :section-numbers      nil
+      :with-toc             nil
+      :html-doctype         "html5"
+      :html-extension       "html"
+      :htmlized-source      t)
+     ("category-theory-svg"
+      :base-directory       ,(expand-file-name (concat org-roam-directory "svg/"))
+      :base-extension       "svg"
+      :publishing-directory ,(file-name-concat default-directory "docs" "svg")
+      :publishing-function  org-publish-attachment)
+     ("category-theory"
+      :components
+      ("category-theory-html"
+       "category-theory-svg")))))
 
 (eval-and-compile
   (defun eb/org-roam-custom-link-builder (node)
